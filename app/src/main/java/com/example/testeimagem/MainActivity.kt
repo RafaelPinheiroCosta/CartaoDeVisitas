f09199cb83fcede80d53a86bea612eca713503a6
Rafael Costa <101342822+RafaelPinheiroCosta@users.noreply.github.com>
2023-04-17 17:00:30 -0300

    Atualizações

--- app/src/main/java/com/example/testeimagem/MainActivity.kt
+++ app/src/main/java/com/example/testeimagem/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
@@ -58,14 +57,11 @@ fun ImagemFundo() {
         Column (
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally
-
         ){
             Cabecalho()
             Rodape()
         }
-
     }
-
 }
 
 @Composable
@@ -77,14 +73,11 @@ fun Cabecalho() {
     ) {
 
         Image(
-
             painter = painterResource(id = R.drawable.senailogo),
             contentDescription = null,
             modifier = Modifier
                 .size(200.dp)
-
         )
-
         Text(
             modifier = Modifier
                 .padding(top = 1.dp),
@@ -102,7 +95,6 @@ fun Cabecalho() {
             fontWeight = FontWeight.Bold
         )
     }
-
 }
 
 @Composable
@@ -110,9 +102,8 @@ fun Rodape() {
 
     Column (
         modifier = Modifier
-            .fillMaxWidth()
             .fillMaxHeight()
-            .padding(16.dp),
+            .padding(10.dp),
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.Start
     ) {
